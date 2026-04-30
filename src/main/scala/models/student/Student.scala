9241ad4a80c8d3f2c16927fbf6a48b90c16b1468
Gonçalo <goncalobranco299@gmail.com>
2026-04-30 14:52:38 +0000

    Project

--- src/main/scala/models/student/Student.scala
+++ src/main/scala/models/student/Student.scala
@@ -1,4 +1,5 @@
 package models.student
+import models.discipline.Discipline
 import zio.json.{DeriveJsonEncoder, JsonEncoder}
 
   case class Student(id: String, label: String, name: String, email: String, studentNumber: String, classId: String)
